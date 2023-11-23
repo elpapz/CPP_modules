f16fc35d4d52e4ea74971286e249590cd87a3370
elpapi <andrecanelaskaldeich@gmail.com>
2023-11-23 06:10:27 +0000

    very first cpp

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+int	main(int ac, char **av)
+{
+	if (ac >= 2)
+	{
+		for(int i = 1; av[i]; i++)
+		{
+			if (i > 1 && av[i])
+				std::cout << " ";
+			for(int j = 0; av[i][j]; j++)
+				std::cout << char(toupper(av[i][j]));
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+}
