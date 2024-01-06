bcd23735d79a078d2e0344f5780cdea038c8567e
elpapi <andrecanelaskaldeich@gmail.com>
2024-01-06 01:56:00 +0000

    lets

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -12,12 +12,43 @@ void	replaceChar(std::string& line, std::string Find, std::string Replace)
 		i = line.find(Find, i);
 		if (i == std::string::npos)
 			break ;
-		line.replace(i, Find.length(), Replace);
+		line.erase(i, Find.length());
+		line.insert(i, Replace);
 		i += Find.length();
 	}
 }
 
 int	main(int ac, char **av)
 {
-	2
+	if (ac != 4)
+	{
+		std::cout << "Wrong number of paremeters!!\n";
+		exit(EXIT_FAILURE);
+	}
+	if (!av[2] || !av[3])
+	{
+		std::cout << "The find and replace strings can't be empty" << std::endl;
+	}
+	std::string line;
+	std::string newFile = av[1];
+	std::string Find = av[2];
+	std::string Replace = av[3];
+	std::ifstream file(av[1]);
+	newFile += ".replace";
+	std::ofstream replaceFile(newFile.c_str());
+	if (!file.is_open() || !replaceFile.is_open())
+	{
+		std::cout << "Something went wrong while opening the files\n";
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		while(std::getline(file, line))
+		{
+			replaceChar(line, Find, Replace);
+			replaceFile << line << std::endl;
+		}
+		file.close();
+		replaceFile.close();
+	}
 }
